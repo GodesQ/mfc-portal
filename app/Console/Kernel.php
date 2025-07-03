8a623fe0e3e172ece5362a09894ba3ff75ad7474
James Garnfil <jamesgarnfil15@gmail.com>
2025-07-03 10:39:43 +0800

    update tithe:send-reminders to five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
     // Run at 10am on the 15th of every month
     $schedule->command('tithe:send-reminders')
-      ->everyTwoMinutes()
+      ->everyFiveMinutes()
       ->timezone('Asia/Manila');
 
   }
